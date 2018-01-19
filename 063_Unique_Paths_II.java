20f4c205f37dcf4c6ef3e49f0492942a5462640a
teppichKay <iamcowa@gmail.com>
2018-01-19 13:32:02 +0200

    063

---
+++ 063_Unique_Paths_II.java
@@ -0,0 +1,19 @@
+class Solution {
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+		int[] res = new int[n];
+		res[0] = 1;
+		
+		for (int i = 0; i < m ;i++) {
+			for (int j = 0; j < n ;j++) {
+				if (obstacleGrid[i][j] == 1) {
+					res[j] = 0;
+				} else if (j > 0){
+					res[j] += res[j-1];
+				}
+			}
+		}
+		return res[n-1];
+	}
+}
