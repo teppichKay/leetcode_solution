52d2b839cd7fbb412f3ce428772f743d4ff40e36
teppichKay <iamcowa@gmail.com>
2018-05-04 23:28:26 +0300

    .

--- src/102_BinaryTreeLevelOrderTraversal.java
+++ src/102_BinaryTreeLevelOrderTraversal.java
@@ -21,6 +21,7 @@
  * }
  */
 
+// DFS
 class Solution {
   public List<List<Integer>> levelOrder(TreeNode root) {
     List<List<Integer>> result = new ArrayList<>();
@@ -40,3 +41,46 @@ class Solution {
     traverse(root.right, level + 1, result);
   }
 }
+
+
+// BFS 
+class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) return res;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      List<Integer> l = new ArrayList<>();
+      int count = queue.size();
+      for (int i = 0; i < count; i++) {
+        TreeNode cur = queue.poll();
+        l.add(cur.val);
+        if (cur.left != null) queue.offer(cur.left);
+        if (cur.right != null) queue.offer(cur.right);
+      }
+      res.add(l);
+    }
+    return res;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
